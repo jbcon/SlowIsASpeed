2299c8f16c3d08865422c810193af5e47798c4c9
John Conover <John Conover>
2016-02-01 13:39:13 -0500

    people go to back of line

--- GottaGoEventually/Assets/Scripts/QueueManager.cs
+++ GottaGoEventually/Assets/Scripts/QueueManager.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class QueueManager : MonoBehaviour {
 
     public static QueueManager singleton;
 
     public GameObject counter;
-    public Person[] people;
+    public PersonSpawner spawn;
+    public List<Person> people;
+    public Person backOfLine;
 
     // space between people
     public float spacing = 0.3f;
@@ -14,32 +17,42 @@ public class QueueManager : MonoBehaviour {
     private Player player;
 
 	// Use this for initialization
-	void Awake()
+	void Start()
     {
         if (singleton == null) singleton = this;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
         counter = GameObject.FindGameObjectWithTag("Counter");
         SpawnPeople();
         people[numberOfPeople - 1].StartMoving();
+        spawn = GameObject.FindGameObjectWithTag("Spawner").GetComponent<PersonSpawner>();
     }
 
 
     void SpawnPeople()
     {
-        people = new Person[numberOfPeople];
-        GameObject person = Resources.Load("Person") as GameObject;
+        people = new List<Person>();
         Person prev = player;
+        people.Add(player);
+        GameObject person = Resources.Load("Person") as GameObject;
         // generate people in line
-        for (int i = 0; i < numberOfPeople; i++)
+        for (int i = 1; i <= numberOfPeople; i++)
         {
             Vector2 newPos = (Vector2)prev.transform.position + Vector2.right * spacing;
             GameObject newP = Instantiate(person);
             newP.transform.position = newPos;
-            people[i] = newP.GetComponent<Person>();
+            people.Add(newP.GetComponent<Person>());
             prev.front = people[i];
             people[i].back = prev;
             prev = people[i];
         }
+        backOfLine = player;
+
+    }
+
+    public void SpawnNewPerson()
+    {
+        Person p = spawn.SpawnPersonBehind(backOfLine);
+        backOfLine = p;
     }
 	
 	// Update is called once per frame
