f67c899d6113c97edf8394398a73e0e07e711b66
John Conover <111jbcon@gmail.com>
2016-02-03 14:51:29 -0500

    ...

---
+++ GottaGoEventually/Assets/ScoreDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public TextMesh text;
+
+    void Start()
+    {
+        text = GetComponent<TextMesh>();
+    }
+    void Update()
+    {
+        text.text = "POINTS: " + ObstacleSpawn.obstaclesSpawned * 1000;
+    }
+}
