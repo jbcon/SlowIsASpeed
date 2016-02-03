544ab43b449ca49e31dff97fc785cf1e18734452
Spencer <johnss18@rpi.edu>
2016-02-03 00:50:21 -0500

    uncomment something before pushing

--- GottaGoEventually/Assets/Scripts/ObstacleSpawn.cs
+++ GottaGoEventually/Assets/Scripts/ObstacleSpawn.cs
@@ -30,14 +30,14 @@ public class ObstacleSpawn : MonoBehaviour
         {
             for (int j = 0; j < numOfSpawners.y; j++)
             {
-                //Transform spawner = Resources.Load<Transform>("Spawner");
-                //spawner = Instantiate<Transform>(spawner);
-                //spawner.name = "Spawner x: " + i + " y: " + j;
-                //spawner.position = new Vector3(spawnerStart.x + i * posModifier.x, spawnerStart.y + j * posModifier.y, 40);
+                Transform spawner = Resources.Load<Transform>("Spawner");
+                spawner = Instantiate<Transform>(spawner);
+                spawner.name = "Spawner x: " + i + " y: " + j;
+                spawner.position = new Vector3(spawnerStart.x + i * posModifier.x, spawnerStart.y + j * posModifier.y, 40);
 
-                //spawners.Add(spawner);
-                //float xDist = i 
-                //spawner.transform = new Vector3();
+                spawners.Add(spawner);
+                //float xDist = i
+               // spawner.transform = new Vector3();
             }
         }
         StartCoroutine("SpawnRandom");
