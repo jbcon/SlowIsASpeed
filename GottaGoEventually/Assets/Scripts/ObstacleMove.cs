c2641dcf293fc36433ee32c4c7bf742ab555b459
Spencer <johnss18@rpi.edu>
2016-02-03 03:43:22 -0500

    plane hitbox is a bit bigger, added spotlight to front of plane

--- GottaGoEventually/Assets/Scripts/ObstacleMove.cs
+++ GottaGoEventually/Assets/Scripts/ObstacleMove.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class ObstacleMove : MonoBehaviour
 {
-    protected float speed = 10;
+    [HideInInspector]
+    public float speed = 10;
 
     public Transform target;
 
 	// Use this for initialization
 	void Start () {
         transform.LookAt(target);
+        //GetComponent<Renderer>().material.color = Color.ler
 	}
 	
 	// Update is called once per frame
@@ -17,6 +19,5 @@ public class ObstacleMove : MonoBehaviour
         transform.Translate(0, 0, speed * Time.deltaTime);
         if (transform.position.z < -10)
             Destroy(gameObject);
-
 	}
 }
